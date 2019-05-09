d7dc1d1a0cc9ec2c69bc0e836e3348f208b32b4f
Manar <mnoooooor@gmail.com>
2019-05-09 17:10:26 +0300

    Initial Commit

---
+++ TheMovieManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieManager
+//
+//  Created by manar AL-Towaim on 09/05/2019.
+//  Copyright © 2019 manar AL-Towaim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
